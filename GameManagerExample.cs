59a37de8552132980e20ffb11d06c0eb925db756
Nicola <nicola.bestm@gmail.com>
2021-09-15 17:14:10 +1000

    Added first files

---
+++ GameManagerExample.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Example of a game manager that utilises the state machine example
+/// </summary>
+/// <remarks>
+/// For scene loading this script will be on a Game Object within a base scene, and scenes will be additively loaded and unloaded from it
+/// This is just an example and will not run in Unity, a more robust additive scene loader async system is needed, this script demonstrates state machines only
+/// </remarks>
+
+namespace NB
+{   
+    public class GameManagerExample : MonoBehaviour
+    {
+        /// <summary>
+        /// Unity SceneManager instance
+        /// </summary>
+        SceneManager sceneManager;
+
+        /// <summary>
+        /// The game state machine
+        /// </summary>
+        StateMachine gameStateMachine = new StateMachine();
+
+        #region Loading State Variables
+
+        private AsyncOperation asyncLoad;
+
+        public GameObject loadingScreen;
+
+        #endregion Loading State Variables
+
+        void Start()
+        {
+            DontDestroyOnLoad(this.gameObject);
+
+            gameStateMachine.ChangeState(new LoadSceneState(this, "Scene1"));
+        }
+
+        private void Update()
+        {
+            gameStateMachine.Update();
+        }
+
+        #region Loading State Functions
+        public void ShowLoadingScreen()
+        {
+            loadingScreen.SetActive(true);
+        }
+
+        public void LoadNewScene(string sceneName)
+        {
+            asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        }
+
+        public bool CheckSceneLoaded()
+        {
+            return asyncLoad.isDone;
+        }
+
+        public void HideLoadingScreen()
+        {
+            loadingScreen.SetActive(false);
+        }
+
+        #endregion Loading State Functions
+
+        #region Run Scene State Functions
+        public void StartGamePlay()
+        {
+            gameStateMachine.ChangeState(new RunSceneState(this));
+        }
+
+        #endregion Run Scene State Functions
+    }
+
+    /// <summary>
+    /// Example script for loading a scene through a state machine
+    /// </summary>
+    public class LoadSceneState : NBIState
+    {
+        GameManagerExample owner;
+
+        string sceneName;
+
+        public LoadSceneState(GameManagerExample owner, string sceneName)
+        {
+            this.owner = owner;
+
+            this.sceneName = sceneName;
+        }
+
+        public void Enter()
+        {
+            Debug.Log("Enter loading scene state");
+
+            owner.ShowLoadingScreen();
+
+            owner.LoadNewScene(sceneName);
+        }
+
+        public void Execute()
+        {
+            Debug.Log("Execute loading scene state");
+
+            if (owner.CheckSceneLoaded())
+            {
+                owner.StartGamePlay();
+            }
+        }
+
+        public void Exit()
+        {
+            Debug.Log("Exit loading scene state");
+
+            owner.HideLoadingScreen();
+        }
+    }
+
+    /// <summary>
+    /// Example script for running scene gameplay through a state machine
+    /// </summary>
+    public class RunSceneState : NBIState
+    {
+        GameManagerExample owner;
+
+        public RunSceneState(GameManagerExample owner)
+        {
+            this.owner = owner;
+        }
+
+        public void Enter()
+        {
+            Debug.Log("Enter run scene state");
+        }
+
+        public void Execute()
+        {
+            Debug.Log("Execute run scene state");
+        }
+
+        public void Exit()
+        {
+            Debug.Log("Exit run scene state");
+        }
+    }
+}
---
+++ StateMachine.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// State machine utility class and state interface to easily run states and their functions
+/// </summary>
+/// <remarks>
+/// Best utilised at the beginning of a project, can replace IEnumerator usage and manage game states
+/// - interface has enter, execute and exit for each state that can be accessed by all classes that inherit from the state machine class
+/// - state machine should be created and utilised through another script, refer to StateMachineExample script
+/// - can be expanded and customised, this really is the basic structure
+/// </remarks>
+
+namespace NB
+{
+    /// <summary>
+    /// A simple state interface
+    /// </summary>
+    /// <remarks>
+    /// Improvements: create wrapper class on the interface which allows greater control over the three functions
+    /// : use delegates in place of functions to allow existing functions in other classes to be used as the enter/execute/exit functions
+    /// </remarks>
+    public interface NBIState
+    {
+        void Enter();
+        void Execute();
+        void Exit();
+    }
+
+    /// <summary>
+    /// A simple state machine class
+    /// </summary>
+    /// <remarks>
+    /// Improvements: keep a list of states rather than load each state in as new, and hold the current state in a separate variable
+    /// : give the state machine (and its states) its own name when you intend to have multiple state machines (which you will) to keep track of each 
+    /// : allow adding, removing, finding current or any states by name in the list stored in the state machine
+    /// </remarks>
+    public class StateMachine
+    {
+        NBIState currentState;
+
+        public void ChangeState(NBIState newState)
+        {
+            if (currentState != null)
+                currentState.Exit();
+
+            currentState = newState;
+            currentState.Enter();
+        }
+
+        public void Update()
+        {
+            if (currentState != null)
+                currentState.Execute();
+        }
+    }
+}
